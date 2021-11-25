99eb25778a0cc64dcaba8b3833a85b48c55844a0
Alexey <vassilyevalexey@gmail.com>
2021-11-25 21:58:34 +0600

    бла бла бла файл

--- src/test/java/guru/qa/tests/HomeWork5_10.java
+++ src/test/java/guru/qa/tests/HomeWork5_10.java
@@ -48,7 +48,7 @@ public class HomeWork5_10 extends TestBase {
             });
             step("Загрузка файла", () -> {
                 //registrationPage.setPicture("p1.png");
-                //registrationPage.setPicture("img/" + picture);
+                registrationPage.setPicture("img/" + picture);
                 registrationPage.setPicture("img\\" + picture);
             });
             step("Заполнение поля адресс", () -> {
