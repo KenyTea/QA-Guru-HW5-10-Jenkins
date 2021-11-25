1b1ba8ffbea97b91931d995df0a48ec431e50756
Alexey <vassilyevalexey@gmail.com>
2021-11-25 22:34:13 +0600

    Обновление на чистовую

--- src/test/java/guru/qa/tests/HomeWork5_10.java
+++ src/test/java/guru/qa/tests/HomeWork5_10.java
@@ -8,7 +8,6 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 
