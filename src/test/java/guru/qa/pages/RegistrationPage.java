df0843c4981490c20864694eff8f82149fed55d2
Alexey <vassilyevalexey@gmail.com>
2021-11-25 11:07:50 +0600

    Навёл красрту с зависимостями

--- src/test/java/guru/qa/pages/RegistrationPage.java
+++ src/test/java/guru/qa/pages/RegistrationPage.java
@@ -1,7 +1,6 @@
 package guru.qa.pages;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import guru.qa.components.CalendarComponent;
 
