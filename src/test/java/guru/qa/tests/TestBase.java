c3050e315d421f7037d672c99f75fd8943e6fb22
Andrey2694 <fordcoppola94@gmail.com>
2021-11-28 12:02:38 +0100

    added properties

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -5,19 +5,27 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.components.RandomFaker;
 import guru.qa.helpers.Attach;
 import guru.qa.pages.RegistrationPage;
+import guru.qa.tests.config.PropertiesConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static java.lang.String.format;
+
 public class TestBase {
     public RegistrationPage registrationPage = new RegistrationPage();
     public RandomFaker randomFaker = new RandomFaker();
+    public PropertiesConfig properties = ConfigFactory.create(PropertiesConfig.class);
+    private final String url = properties.url();
+    private final String login = properties.login();
+    private final String password = properties.password();
 
     @BeforeEach
    public void setup() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = format("https://%s:%s@%s", login, password, url);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
